892a9620b0df48e27c16dce96cd1e8a44fa56d4a
davidrockkj <davidrockkj@gmail.com>
2023-04-24 17:53:27 -0300

    Finalizando projeto

--- public/src/index.ts
+++ public/src/index.ts
@@ -1,5 +1,39 @@
 // IIFE - Immediately Invoked Function Expression - coloca todas as variáveis dentro do scope local
 (() => {
+    // Criando o Enum para poder listar todas as maneiras de notificar o usuário
+    enum NotificationPlatform {
+        SMS = 'SMS',
+        EMAIL = 'EMAIL',
+        PUSH_NOTIFICATION = 'PUSH_NOTIFICATION',
+    }
+
+    // Identificar qual o tipo de task estamos criando
+    enum ViewMode {
+        TODO = 'TODO',
+        REMINDER = 'REMINDER',
+    }
+
+    // Criando função que gera um ID único para o reminder e o todo
+    const UUID = (): string => {
+        return Math.random().toString(32).substr(2, 9);
+    };
+
+    // Função para retornar uma data entendível
+    const DateUtils = {
+        tomorrow(): Date {
+            const tomorrow = new Date();
+            tomorrow.setDate(tomorrow.getDate() + 1);
+            return tomorrow;
+        },
+        today(): Date {
+            return new Date();
+        },
+        // Função para formatar a data
+        formatDate(date: Date): string {
+            return `${date.getDate()}.${date.getMonth() + 1}.${date.getFullYear()}`;
+        }
+    };
+
     interface Task{// Definindo a interface
         id: string; // nome da anotação
         dateCreated: Date; // dia em que foi criado
@@ -13,14 +47,14 @@
     // todo e reminder serão classes que implementarão a interface
     // classe REMINDER
     class Reminder implements Task {
-        id: string = ''; // recebe nulo, por enquanto
-        dateCreated: Date = new Date();
-        dateUpdated: Date = new Date();
+        id: string = UUID(); // recebe nulo, por enquanto
+        dateCreated: Date = DateUtils.today();
+        dateUpdated: Date = DateUtils.today();
         description: string = '';
 
         // Coisas específicas para o reminder (e não para o todo)
-        date: Date = new Date();
-        notifications: Array<string> = ['EMAIL'] 
+        date: Date = DateUtils.tomorrow();
+        notifications: Array<NotificationPlatform> = [NotificationPlatform.EMAIL] 
         // onde o usuário vai receber a notificação
         // por padrão, o local de notificação está EMAIL
 
@@ -28,7 +62,7 @@
         constructor(
             description: string,
             date: Date,
-            notifications: Array<string>
+            notifications: Array<NotificationPlatform>
         ) {
             this.description = description;
             this.date = date;
@@ -37,8 +71,13 @@
         }
 
         render(): string {
-            return JSON.stringify(this);
-            // this é a referencia da classe -> a instancia do objeto que foi criado
+            // Retornando uma string template
+            return `
+            ---> Reminder <---
+            Description: ${this.description}
+            Date: ${DateUtils.formatDate(this.date)}
+            Notification: ${this.notifications.join(',')}
+            `;
         }
         // nesse caso o método render() além de definido, está implementado
         // O método render() irá servir para definir como o reminder e o todo devem aparecer na tela
@@ -47,9 +86,9 @@
 
     // classe TODO
     class Todo implements Task {
-        id: string = '';
-        dateCreated: Date = new Date();
-        dateUpdated: Date = new Date();
+        id: string = UUID();
+        dateCreated: Date = DateUtils.today();
+        dateUpdated: Date = DateUtils.today();
         description: string = '';
 
         done: boolean = false;
@@ -59,7 +98,11 @@
         }
 
         render(): string {
-            return JSON.stringify(this);
+            return `
+            ---> TODO <---
+            Description: ${this.description}
+            Done: ${this.done}
+            `;
         }
 
     }
@@ -69,12 +112,39 @@
     // constante que recebe objeto
     const todo = new Todo('Todo criado com a classe');
 
-    const reminder = new Reminder('Reminder criado com a classe', new Date(), ['EMAIL']);
+    const reminder = new Reminder('Reminder criado com a classe', new Date(), [NotificationPlatform.EMAIL]);
 
     // view - ponto em que o código se comunica com a interface - renderizar os todos e reminders
     const taskView = {
+        // Receber o elemento do formulário e retornar um TODO ou um REMINDER
+        
+        getTodo(form: HTMLFormElement): Todo {
+
+            // constante que recebe o valor digitado
+            const todoDescription = form.todoDescription.value;
+            // resetando o formulário
+            form.reset();
+            // criar o todo com o 'new todo' passando a descrição
+            return new Todo(todoDescription); 
+        },
+
+        // Recebe um formulário e retorna um reminder
+        getReminder(form: HTMLFormElement): Reminder {
+            const reminderNotifications = [
+                    form.notifications.value as NotificationPlatform,
+            ];
+            const reminderDate = new Date(form.reminderDate.value);
+            const reminderDescription = form.reminderDescription.value;
+            form.reset();
+            return new Reminder(
+                reminderDescription,
+                reminderDate,
+                reminderNotifications
+            );
+        },
+
         // lista com as anotações salvas
-        render(tasks: Array<Task>) {
+        render(tasks: Array<Task>, mode: ViewMode) {
 
             // processo de limpar a lista - evitar a duplicidade dos elementos
             const taskList = document.getElementById('taskList') // seletor que pega o elemento de lista, que tem o ID taskList
@@ -91,17 +161,68 @@
                 li.appendChild(textNode);
                 taskList?.appendChild(li);
             });
+
+            // Código que faz a troca do modo // inputs do formulário de TODO e REMINDER
+            const todoSet = document.getElementById('todoSet');
+            const reminderSet = document.getElementById('reminderSet');
+
+            // Validando o modo 'ViewMode'
+            if (mode === ViewMode.TODO) {
+                todoSet?.setAttribute('style', 'display: block');
+                todoSet?.removeAttribute('disabled');
+                reminderSet?.setAttribute('style', 'display: none');
+                reminderSet?.setAttribute('disabled', 'true');
+            } else {
+                reminderSet?.setAttribute('style', 'display: block');
+                reminderSet?.removeAttribute('disabled');
+                todoSet?.setAttribute('style', 'display: none');
+                todoSet?.setAttribute('disabled', 'true');  
+            }
+
         },
     };
 
     // Controller que mantem os todos e reminders em memória
     const TaskController = (view: typeof taskView) => { // Garantir quando a nossa View deve renderizar e armazenar em memória dentro do navegador as tasks
-        const tasks: Array<Task> = [todo, reminder]; // como o controller vai manter a lista em memória
+        const tasks: Array<Task> = []; // como o controller vai manter a lista em memória
+
+        // Identificar qual o tipo de task estamos criando
+        let mode: ViewMode = ViewMode.TODO;
 
         const handleEvent = (event: Event) => {
             event.preventDefault(); // Prevenindo o evento Default
-            view.render(tasks);
+            const form = event.target as HTMLFormElement;
+            switch (mode as ViewMode) {
+                case ViewMode.TODO:
+                    tasks.push(view.getTodo(form));
+                    break;
+                case ViewMode.REMINDER:
+                    tasks.push(view.getReminder(form));
+                    break;
+            }
+            view.render(tasks, mode);
         };
+
+        // Alterar o modo entre TODO e REMINDER quando apertar no Toggle
+        const handleToggleMode = () => {
+            switch (mode as ViewMode) {
+                case ViewMode.TODO:
+                    mode = ViewMode.REMINDER
+                    break;
+            
+                case ViewMode.REMINDER:
+                    mode = ViewMode.TODO
+                    break
+            }
+            view.render(tasks, mode)
+        }
+
+
+        // Declarando o 'event listener' para o botão de 'Toggle Mode'
+        // para conseguir trocar o formulário e passar para ele a função 'handleToggleMode'
+        document
+            .getElementById('toggleMode')
+            ?.addEventListener('click', handleToggleMode);
 
         document
         .getElementById('taskForm')
